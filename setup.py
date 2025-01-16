9ae3c688b42c3161079dd5f1663d3c8b8048cc9b
Gaurav <gauravbosamiya9@gmail.com>
2025-01-16 20:01:08 +0530

    project structured added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Gaurav Bosamiya',
+    author_email='gauravbosamiya9@gmail.com',
+    packages=find_packages()
+)
